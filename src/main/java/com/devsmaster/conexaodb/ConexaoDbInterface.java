e83f7342c1ff652a3a5f66d6a1443521cc62e7a2
Marcos Vinícius Santos Souza <Marcos.viniciusk8@hotmail.com>
2020-10-10 09:46:27 -0300

    Alterado o nome do autor que se refere da minha maquina para o meu nome

--- src/main/java/com/devsmaster/conexaodb/ConexaoDbInterface.java
+++ src/main/java/com/devsmaster/conexaodb/ConexaoDbInterface.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 
 /**
  *
- * @author Azazel
+ * @author Marcos Vinicius Santos Souza
  */
 public interface ConexaoDbInterface {
 
--- src/main/java/com/devsmaster/conexaodb/ConexaoMysql.java
+++ src/main/java/com/devsmaster/conexaodb/ConexaoMysql.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 
 /**
  *
- * @author Azazel
+ * @author Marcos Vinicius Santos Souza
  */
 public class ConexaoMysql implements ConexaoDbInterface {
 
