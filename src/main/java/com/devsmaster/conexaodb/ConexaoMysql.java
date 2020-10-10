b1a1009dc11a9f17d2f991c0e9ae89d0671aeafd
Marcos Dead <Marcos.viniciusk8@hotmail.com>
2020-10-09 22:57:54 -0300

    Classe com conexão com o banco de dados mysql

---
+++ src/main/java/com/devsmaster/conexaodb/ConexaoMysql.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.devsmaster.conexaodb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Azazel
+ */
+public class ConexaoMysql implements ConexaoDbInterface {
+
+    private static String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    private static String SERVER = "localhost";
+    private static String DATABASE = "teste";
+
+    private static String LOGIN = "root";
+    private static String SENHA = "Bemvindo1";
+
+    private static String URL = "";
+
+    private static Connection CONEXAO = null;
+
+    @Override
+    public Connection openConnection() throws ClassNotFoundException, SQLException {
+        URL = "jdbc:mysql://" + SERVER + ":3306/" + DATABASE + "?useTimezone=true&serverTimezone=UTC&useSSL=false";
+
+        Class.forName(DRIVER);
+        CONEXAO = DriverManager.getConnection(URL, LOGIN, SENHA);
+
+        return CONEXAO;
+    }
+
+    @Override
+    public boolean closeConnection() throws ClassNotFoundException, SQLException {
+        boolean retorno = false;
+
+        if (CONEXAO != null) {
+            if (!CONEXAO.isClosed()) {
+                CONEXAO.close();
+                retorno = true;
+            }
+        }
+
+        return retorno;
+    }
+}
