caeebd35549ec68e70ca52e682e0fddcb09146ac
Marcos Vinícius Santos Souza <Marcos.viniciusk8@hotmail.com>
2020-10-17 13:11:07 -0300

    Desenvolvido classe pessoa que vai ser a classe para outras classes herdarem seus atributos

---
+++ src/main/java/br/senac/sp/entidade/Pessoa.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.sp.entidade;
+
+/**
+ *
+ * @author Azazel
+ */
+public class Pessoa {
+
+    private String nome;
+    private String sexo;
+    private String data_nascimento;
+    private String cpf;
+    private String rua;
+    private String cep;
+    private int numero_casa;
+    private String bairro;
+    private String email;
+    private String telefone;
+
+    public Pessoa() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(String data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public int getNumero_casa() {
+        return numero_casa;
+    }
+
+    public void setNumero_casa(int numero_casa) {
+        this.numero_casa = numero_casa;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+}
