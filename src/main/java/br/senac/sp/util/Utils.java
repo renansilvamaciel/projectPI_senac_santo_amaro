0126605c5d5926fa1420ef3952f6ba34d89df69c
Marcos Vinícius Santos Souza <Marcos.viniciusk8@hotmail.com>
2020-10-17 13:12:37 -0300

    Classe que contem mensagem de erro ou de sucesso para quando efetuarmos alguma ação

---
+++ src/main/java/br/senac/sp/util/Utils.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.sp.util;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Azazel
+ */
+public class Utils {
+    
+     public static void mostrarTelaErro(Exception ex, 
+            HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String msgErro = ex.getMessage();
+        request.setAttribute("msgErro", msgErro);
+        RequestDispatcher requestDispatcher = request
+                 .getRequestDispatcher("/Erro.jsp");
+        requestDispatcher.forward(request, response);
+    }
+    
+    public static void mostrarTelaSucesso(HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect("Success.jsp");
+    }
+}
