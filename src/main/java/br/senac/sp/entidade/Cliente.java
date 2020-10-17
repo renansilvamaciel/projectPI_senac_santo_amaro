d3c7b50307056b8bab601d7e4a5caa55342bbd2f
Marcos Vinícius Santos Souza <Marcos.viniciusk8@hotmail.com>
2020-10-17 13:12:18 -0300

    Classe do cliente que extende os atributos de pessoa

---
+++ src/main/java/br/senac/sp/entidade/Cliente.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.sp.entidade;
+
+/**
+ *
+ * @author Azazel
+ */
+public class Cliente extends Pessoa {
+
+    private int id_cliente;
+    private String assinatura;
+
+    public Cliente() {
+    }
+
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getAssinatura() {
+        return assinatura;
+    }
+
+    public void setAssinatura(String assinatura) {
+        this.assinatura = assinatura;
+    }
+
+}
