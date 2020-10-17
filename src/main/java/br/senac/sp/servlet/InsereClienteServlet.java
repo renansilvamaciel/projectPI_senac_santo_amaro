2c6a4cdcaf7f1d49b79c6fad0f0d71440c504f57
Marcos Vinícius Santos Souza <Marcos.viniciusk8@hotmail.com>
2020-10-17 13:38:21 -0300

    servlet para inserir cliente

---
+++ src/main/java/br/senac/sp/servlet/InsereClienteServlet.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.sp.servlet;
+
+import br.senac.sp.dao.ClientesDAO;
+import br.senac.sp.entidade.Cliente;
+import br.senac.sp.util.Utils;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Azazel
+ */
+public class InsereClienteServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, NumberFormatException {
+
+        Cliente cliente = new Cliente();
+
+        cliente.setNome(request.getParameter("nome"));
+        cliente.setSexo(request.getParameter("sexo"));
+        cliente.setData_nascimento(request.getParameter("data_nascimento"));
+        cliente.setCpf(request.getParameter("cpf"));
+        cliente.setRua(request.getParameter("rua"));
+        cliente.setCep(request.getParameter("cep"));
+        cliente.setNumero_casa(Integer.parseInt(request.getParameter("numero_casa")));
+        cliente.setBairro(request.getParameter("bairro"));
+        cliente.setEmail(request.getParameter("email"));
+        cliente.setTelefone(request.getParameter("telefone"));
+        cliente.setAssinatura(request.getParameter("assinatura"));
+
+        try {
+            ClientesDAO clienteDao = new ClientesDAO();
+            clienteDao.insertClient(cliente);
+            response.sendRedirect("Success.jsp");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(InsereClienteServlet.class.getName()).log(Level.SEVERE, null, ex);
+            Utils.mostrarTelaErro(ex, request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(InsereClienteServlet.class.getName()).log(Level.SEVERE, null, ex);
+            Utils.mostrarTelaErro(ex, request, response);
+        }
+
+    }
+
+}
