36d9d3acc8431e884e065127fe7ff49786573325
Marcos Vinícius Santos Souza <Marcos.viniciusk8@hotmail.com>
2020-10-18 18:35:59 -0300

    Classe de atributos da venda dos planos

---
+++ src/main/java/br/senac/sp/entidade/VendaPlanos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.sp.entidade;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author Azazel
+ */
+@Getter
+@Setter
+public class VendaPlanos {
+   private int id_venda;
+   private String cpf;
+   private int id_vendedor;
+   private String nome;
+   private String tipo_assinatura;
+   private int id_filial;
+    
+}
