4fac78be148eb26dee5cce22358d6a56566a90c8
Marcos Vinícius Santos Souza <Marcos.viniciusk8@hotmail.com>
2020-10-18 22:13:47 -0300

    Insercao no banco de dados, ainda não esta pronto

---
+++ src/main/java/br/senac/sp/dao/VendaPlanoDAO.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.sp.dao;
+
+import br.senac.sp.conexaodb.ConexaoMysql;
+import br.senac.sp.entidade.VendaPlanos;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Azazel
+ */
+public class VendaPlanoDAO {
+
+    public boolean insertClient(VendaPlanos vendaPlanos) throws ClassNotFoundException, SQLException {
+        boolean success = false;
+        ConexaoMysql connectionMysql = new ConexaoMysql();
+        Connection connection = connectionMysql.openConnection();
+        String query = "insert into venda_plano (cpf, id_vendedor, assinatura, tipo_assinatura, id_filial) values (?, ?, ?, ?, ?)";
+        PreparedStatement instructionSql = connection.prepareCall(query);
+
+        
+        instructionSql.setString(1, vendaPlanos.getCpf());
+        instructionSql.setInt(2, vendaPlanos.getId_vendedor());
+        instructionSql.setString(3, vendaPlanos.getAssinatura());
+        instructionSql.setString(4, vendaPlanos.getTipo_assinatura());
+        instructionSql.setInt(5, vendaPlanos.getId_filial());
+
+        if (instructionSql.executeUpdate() >= 1) {
+            success = true;
+        }
+
+        closeStatementAndCloseConnection(instructionSql);
+        connectionMysql.closeConnection();
+
+        return success;
+    }
+
+    private void closeStatementAndCloseConnection(PreparedStatement preparedStatement) throws ClassNotFoundException {
+        if (preparedStatement != null) {
+            try {
+                preparedStatement.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(ClientesDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+    }
+}
