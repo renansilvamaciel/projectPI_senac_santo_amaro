7c29ed42c6b7eb149467b5084da2cdc9d10d4e96
Marcos Vinícius Santos Souza <Marcos.viniciusk8@hotmail.com>
2020-10-19 22:30:08 -0300

    Logica da venda dos planos

--- src/main/java/br/senac/sp/entidade/VendaPlanos.java
+++ src/main/java/br/senac/sp/entidade/VendaPlanos.java
@@ -22,6 +22,12 @@ public class VendaPlanos {
     private String assinatura;
     private String tipo_assinatura;
     private int id_filial;
+    private final double descontoMensal = 0.10;
+    private final double descontoSemestral = 0.15;
+    private final double descontoAnual = 0.20;
+    private final int valorBasePlanoBronze = 225;
+    private final int valorBasePlanoPrata = 99;
+    private final int valorBasePlanoOuro = 324;
 
     public boolean validationClient(String cpf) {
         boolean success = false;
@@ -30,5 +36,49 @@ public class VendaPlanos {
         }
 
         return success;
+    }
+
+    public double validarValorPlano(String assinatura, String tipo_assinatura) {
+        double valorPlanoEscolhido = 0.0;
+
+        switch (assinatura) {
+            case "ouro":
+
+                if (tipo_assinatura.equalsIgnoreCase("mensal")) {
+                    valorPlanoEscolhido = valorBasePlanoOuro * descontoMensal;
+                } else if (tipo_assinatura.equalsIgnoreCase("semestral")) {
+                    valorPlanoEscolhido = valorBasePlanoOuro * descontoSemestral;
+                } else {
+                    valorPlanoEscolhido = valorBasePlanoOuro * descontoAnual;
+                }
+                break;
+
+            case "prata":
+
+                if (tipo_assinatura.equalsIgnoreCase("mensal")) {
+                    valorPlanoEscolhido = valorBasePlanoPrata * descontoMensal;
+                } else if (tipo_assinatura.equalsIgnoreCase("semestral")) {
+                    valorPlanoEscolhido = valorBasePlanoPrata * descontoSemestral;
+                } else {
+                    valorPlanoEscolhido = valorBasePlanoPrata * descontoAnual;
+                }
+                break;
+
+            case "bronze":
+
+                if (tipo_assinatura.equalsIgnoreCase("mensal")) {
+                    valorPlanoEscolhido = valorBasePlanoBronze * descontoMensal;
+                } else if (tipo_assinatura.equalsIgnoreCase("semestral")) {
+                    valorPlanoEscolhido = valorBasePlanoBronze * descontoSemestral;
+                } else {
+                    valorPlanoEscolhido = valorBasePlanoBronze * descontoAnual;
+                }
+                break;
+
+            default:
+                break;
+        }
+
+        return valorPlanoEscolhido;
     }
 }
