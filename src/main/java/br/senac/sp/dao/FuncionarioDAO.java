5a64806d1a9d5354adbd145b89ac00ae3799994e
Marcos Vinícius Santos Souza <Marcos.viniciusk8@hotmail.com>
2020-11-28 15:53:57 -0300

    Espacamentos

--- src/main/java/br/senac/sp/dao/FuncionarioDAO.java
+++ src/main/java/br/senac/sp/dao/FuncionarioDAO.java
@@ -21,16 +21,16 @@ import java.util.logging.Logger;
  * @author Azazel
  */
 public class FuncionarioDAO {
-    
+
     public boolean insertFuncionario(Funcionario funcionario) throws ClassNotFoundException, SQLException {
         boolean success = false;
         ConexaoMysql connectionMysql = new ConexaoMysql();
         Connection connection = connectionMysql.openConnection();
         String query = "insert into funcionario (nome, sexo, data_nascimento, cpf, rua, cep, numero_casa, bairro, email, telefone, senha, id_filial, cargo) values ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         PreparedStatement instructionSql = connection.prepareCall(query);
-        
+
         funcionario.setFilial(1);
-        
+
         instructionSql.setString(1, funcionario.getNome());
         instructionSql.setString(2, funcionario.getSexo());
         instructionSql.setString(3, funcionario.getData_nascimento());
@@ -44,27 +44,27 @@ public class FuncionarioDAO {
         instructionSql.setString(11, funcionario.getSenha());
         instructionSql.setInt(12, funcionario.getFilial());
         instructionSql.setString(13, funcionario.getCargo());
-        
+
         if (instructionSql.executeUpdate() >= 1) {
             success = true;
         }
-        
+
         closeStatementAndCloseConnection(instructionSql);
         connectionMysql.closeConnection();
-        
+
         return success;
     }
-    
+
     public ArrayList<Funcionario> listFuncionarios() throws SQLException, ClassNotFoundException {
         ArrayList<Funcionario> listarFuncionarios = new ArrayList<>();
-        
+
         ConexaoMysql connectionMysql = new ConexaoMysql();
         Connection connection = connectionMysql.openConnection();
         String query = "select * from funcionario order by id_funcionario";
         PreparedStatement instructionSql = connection.prepareCall(query);
-        
+
         ResultSet result = instructionSql.executeQuery();
-        
+
         while (result.next()) {
             Funcionario funcionario = new Funcionario();
             funcionario.setId_funcionario(result.getInt("id_funcionario"));
@@ -80,15 +80,15 @@ public class FuncionarioDAO {
             funcionario.setTelefone(result.getString("telefone"));
             funcionario.setFilial(result.getInt("id_filial"));
             funcionario.setCargo(result.getString("cargo"));
-            
+
             listarFuncionarios.add(funcionario);
         }
-        
+
         closeStatementAndCloseConnection(instructionSql);
         connectionMysql.closeConnection();
         return listarFuncionarios;
     }
-    
+
     public ArrayList<Funcionario> getFuncionario(Funcionario funcionario) throws ClassNotFoundException, SQLException {
         ArrayList<Funcionario> listarFuncionario = new ArrayList<>();
         ConexaoMysql connectionMysql = new ConexaoMysql();
@@ -98,7 +98,7 @@ public class FuncionarioDAO {
         instructionSql.setString(1, funcionario.getCpf());
         instructionSql.setInt(2, funcionario.getId_funcionario());
         instructionSql.setString(3, "%" + funcionario.getNome() + '%');
-        
+
         ResultSet result = instructionSql.executeQuery();
         if (result.next()) {
             funcionario.setId_funcionario(result.getInt("id_funcionario"));
@@ -114,22 +114,22 @@ public class FuncionarioDAO {
             funcionario.setTelefone(result.getString("telefone"));
             funcionario.setFilial(result.getInt("id_filial"));
             funcionario.setCargo(result.getString("cargo"));
-            
+
             listarFuncionario.add(funcionario);
         }
         closeStatementAndCloseConnection(instructionSql);
-        
+
         connectionMysql.closeConnection();
         return listarFuncionario;
     }
-    
+
     public boolean updateFuncionario(Funcionario funcionario) throws ClassNotFoundException, SQLException {
         boolean success = false;
         ConexaoMysql connectionMysql = new ConexaoMysql();
         Connection connection = connectionMysql.openConnection();
         String query = "update funcionario set nome=?, sexo=?, data_nascimento=?, rua=?, cep=?, numero_casa=?, bairro=?, email=?, telefone=?, senha=?, id_filial=?, cargo=? where cpf=?";
         PreparedStatement instructionSql = connection.prepareCall(query);
-        
+
         instructionSql.setString(1, funcionario.getNome());
         instructionSql.setString(2, funcionario.getSexo());
         instructionSql.setString(3, funcionario.getData_nascimento());
@@ -143,68 +143,68 @@ public class FuncionarioDAO {
         instructionSql.setInt(11, funcionario.getFilial());
         instructionSql.setString(12, funcionario.getCargo());
         instructionSql.setString(13, funcionario.getCpf());
-        
+
         if (instructionSql.execute()) {
             success = true;
         }
-        
+
         closeStatementAndCloseConnection(instructionSql);
         connectionMysql.closeConnection();
         return success;
     }
-    
+
     public boolean deleteFuncionario(String cpf) throws ClassNotFoundException, SQLException {
         boolean success = false;
         ConexaoMysql connectionMysql = new ConexaoMysql();
         Connection connection = connectionMysql.openConnection();
         String query = "delete from funcionario where cpf=?";
         PreparedStatement instructionSql = connection.prepareCall(query);
-        
+
         instructionSql.setString(1, cpf);
         int linhasAfetadas = instructionSql.executeUpdate();
-        
+
         if (linhasAfetadas > 0) {
             success = true;
         }
-        
+
         closeStatementAndCloseConnection(instructionSql);
         connectionMysql.closeConnection();
         return success;
     }
-    
+
     public Funcionario login(String cpf) throws ClassNotFoundException, SQLException {
         ConexaoMysql connectionMysql = new ConexaoMysql();
         Connection connection = connectionMysql.openConnection();
         String query = "select * from funcionario where cpf=?";
         PreparedStatement instructionSql = connection.prepareCall(query);
         Funcionario funcionario = new Funcionario();
-        
+
         instructionSql.setString(1, cpf);
-        
+
         ResultSet result = instructionSql.executeQuery();
-        
+
         if (result.next()) {
             funcionario.setCpf(result.getString("cpf"));
             funcionario.setSenha(result.getString("senha"));
         }
-        
+
         closeStatementAndCloseConnection(instructionSql);
         connectionMysql.closeConnection();
-        
+
         return funcionario;
     }
-    
+
     private void closeStatementAndCloseConnection(PreparedStatement preparedStatement) throws ClassNotFoundException {
         if (preparedStatement != null) {
             try {
                 preparedStatement.close();
-                
+
             } catch (SQLException ex) {
                 Logger.getLogger(ClientesDAO.class
                         .getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
+
     }
-    
+
 }
